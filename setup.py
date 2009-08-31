1f004529f1cd5f2895bd2965671fc2b13ba6a985
Henrik Gustafsson <henrik.gustafsson@fnord.se>
2009-08-31 16:49:54 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name = "affinity",
+    version = "0.1",
+    author = "Henrik Gustafsson",
+    author_email = "henrik.gustafsson@fnord.se",
+    url = "http://github.com/gsson/affinity",
+    description = "Manipulate process CPU affinity from Python",
+    keywords = "affinity",
+    packages = ["affinity"]
+)
